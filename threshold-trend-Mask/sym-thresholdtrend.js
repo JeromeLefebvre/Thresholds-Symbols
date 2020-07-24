4e45e64c8fdb9e3e6d228e4378477c568c16c6ef
Jerome Lefebvre <jerome.p.lefebvre@gmail.com>
2020-07-24 14:56:37 +0900

    Remove some of the weridness in resizing the symbol

--- threshold-trend-Mask/sym-thresholdtrend.js
+++ threshold-trend-Mask/sym-thresholdtrend.js
@@ -1,9 +1,7 @@
 (function (PV) {
 	'use strict';
-
 	function symbolVis() { }
     PV.deriveVisualizationFromBase(symbolVis);
-
     var definition = {
         typeName: 'thresholdtrend',
         visObjectType: symbolVis,
@@ -17,18 +15,11 @@
                 Width: 500
             };
         },
-        StateVariables: ['MultistateColor'],
-        configOptions: function () {
-            return [{
-                title: 'Configure Threshold Trend',
-                mode: 'formatWindArrow'
-            }];
-        }
+        StateVariables: ['MultistateColor']
     };
     symbolVis.prototype.init = function (scope, elem) {
         scope.canvas = elem.find(".thresholdtrend")[0];
         scope.ctx = scope.canvas.getContext("2d");
-
         this.onDataUpdate = onDataUpdate;
         this.onResize = onResize;
         this.onConfigChange = onConfigChange;
@@ -37,22 +28,18 @@
             scope.ctx.save();
             scope.ctx.globalCompositeOperation = "source-atop";
 
-            scope.ctx.fillStyle = scope.errorColor;
-            scope.ctx.fillRect(0, 0, scope.ctx.canvas.width*scope.fudgeWidth, scope.ctx.canvas.height*scope.fudgeHeight);
+            scope.ctx.fillStyle = (scope.errorColor) ? scope.errorColor : "red";
+            scope.ctx.fillRect(0, 0, 100, 100);
+            var globaltresholds = scope.latestData.Traces.map(thresholds => thresholds.LineSegments[0].split(" ").map(word => word.split(',')));
 
-            var globaltresholdsraw = scope.latestData.Traces.slice(1, scope.latestData.Traces.length);
-            var globaltresholds = globaltresholdsraw.map(thresholds => thresholds.LineSegments[0].split(" ").map(word => word.split(',')));
-
-            for (var globalIndex = 1; globalIndex < globaltresholds.length; globalIndex++) {
-                scope.ctx.fillStyle = scope.colors[globalIndex];
-              
-                // draw the polygon by starting from the bottom, and the the top
+            for (var globalIndex = 2; globalIndex < globaltresholds.length; globalIndex++) {
+                // draw the polygon by starting from the bottom limit, and the the top
                 scope.ctx.beginPath();
                 scope.ctx.moveTo(globaltresholds[globalIndex-1][0][0], globaltresholds[globalIndex-1][0][1]);
                 globaltresholds[globalIndex - 1].forEach(point => scope.ctx.lineTo(point[0], point[1]));
-
                 globaltresholds[globalIndex].slice().reverse().forEach(point => scope.ctx.lineTo(point[0], point[1]));
                 scope.ctx.closePath();
+                scope.ctx.fillStyle = scope.colors[globalIndex];                
                 scope.ctx.fill();
               }
               scope.ctx.restore();
@@ -61,7 +48,6 @@
         function drawCurve() {
             // TODO: Deal with bad data by looping over multiple linesegements
             var points = scope.latestData.Traces[0].LineSegments[0].split(" ").map(p => p.split(','));
-            scope.ctx.setLineDash([]);
             scope.ctx.strokeStyle = "white";
             scope.ctx.lineWidth = 0.1;            
             scope.ctx.beginPath();
@@ -72,24 +58,19 @@
         }
 
         function draw() {
-            //Cear the shadow of past equations
-            scope.ctx.clearRect(0, 0, Math.max(scope.width, 100), scope.height*scope.fudgeHeight);
+            scope.ctx.clearRect(0,0,100,100); // Clear the shadow of past equations
             drawCurve()
-            if (scope.symbol.DataSources.length > 1) {
-                drawColors();
-            }
+            drawColors();
         }
 
         function onDataUpdate(newData) {
-            if (!newData) {
-                return;
-            }
+            if (!newData) return;
             scope.latestData = newData;
             draw();
         }
 
-       // Process configuration changes 
        function onConfigChange(newConfig) {
+            // adds the limits as traces as we need to know the values along the path
             if (scope.symbol.DataSources.length == 1 && scope.symbol.Configuration.Multistates) {
                 scope.symbol.DataSources.push(scope.symbol.DataSources[0] + "|" + "Minimum");
                 newConfig.Multistates[0].States.forEach(
@@ -98,26 +79,19 @@
             }
 
             if (scope.symbol.Configuration.Multistates) {
-                scope.colors = [];
-                scope.symbol.Configuration.Multistates[0].States.forEach(state => scope.colors.push(state.StateValues[0]));
+                scope.colors = Array.from(scope.symbol.Configuration.Multistates[0].States, state => state.StateValues[0]);
                 scope.errorColor = newConfig.Multistates[0].ErrorStateValues[0];
             }   
         }
 
         function onResize(width, height) {            
-            scope.height = height;
-            scope.width = width;
-
-            scope.fudgeHeight = Math.max(1, 1/(height*0.01));
-            scope.fudgeWidth = Math.max(1, 1/(width*0.01));
-            scope.ctx.canvas.width = width;
-            scope.ctx.canvas.height = height;
-            
+            scope.canvas.width = width;
+            scope.canvas.height = height;
             scope.ctx.translate(0, scope.canvas.height);
             scope.ctx.scale(width*0.01, -1*height*0.01);
-    
             clearTimeout(scope.redrawit);
-            scope.redrawit = setTimeout(draw, 100);
+            if (scope.latestData)
+                scope.redrawit = setTimeout(draw, 100);
         }
     };  
 	PV.symbolCatalog.register(definition); 
