d9b421c642bfeb22ee131035c965ffd43cf01507
Jerome Lefebvre <jerome.p.lefebvre@gmail.com>
2020-09-04 17:45:21 +0900

    Install the dependencies for karma

---
+++ install.bat
@@ -0,0 +1,14 @@
+REM Karma itself
+npm install karma --save-dev
+
+REM Installs jasmine testing javascript module
+npm install karma-jasmine jasmine-core --save-dev
+
+REM Chrome headless
+npm install karma-chrome-launcher --save-dev
+
+REM Karma's command line helper
+npm install -g karma-cli
+
+REM Reporter
+npm install karma-spec-reporter --save-dev
